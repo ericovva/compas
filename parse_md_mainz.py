b566f65dd6672437797e360094bb79cc5060078e
Eric <ericovva@yandex.ru>
2019-09-30 15:53:46 +0000

    first commit

---
+++ parse_md_mainz.py
@@ -0,0 +1,24 @@
+from dbfread import DBF
+import psycopg2
+
+connection = psycopg2.connect(user = "bastion",
+							  password = "qwerty7gas",
+							  host = "127.0.0.1",
+							  port = "5432",
+							  database = "astz")
+
+for record in DBF('/home/bastion/MD_MAINZ-20190827/MD_mainz.DBF'):
+    real_keys = []
+    values = []
+    for key in record.keys():
+        val = record[key]
+        if key == 'IS':
+            key = 'IS_'
+        real_keys.append(key)
+        values.append(val)
+
+    cursor = connection.cursor()
+    #cursor.execute("INSERT INTO tz ({}) VALUES ({});".format(','.join(real_keys), ','.join(map(lambda x: u"{}".format(x) or 'null', values))))
+    print(values)
+    record = cursor.fetchone()
+    break
